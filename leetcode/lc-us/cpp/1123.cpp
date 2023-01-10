333f169492a0d537370ad10ed4fd2badd2d1cfe4
Chintsai Hwo <jcglqmoyx@gmail.com>
2023-01-10 11:45:22 +0800

    update

--- leetcode/lc-us/cpp/1123.cpp
+++ leetcode/lc-us/cpp/1123.cpp
@@ -15,10 +15,14 @@ struct TreeNode {
 };
 
 class Solution {
-    pair<TreeNode *, int> dfs(TreeNode *root) {
-        if (!root) return {nullptr, 0};
-        auto l = dfs(root->left), r = dfs(root->right);
-        if (l.second == r.second) return {root, l.second + 1};
+    /*
+     * param node: the root of the tree
+     * return: the lowest common ancestor of the deepest leaves of node, the depth of node
+     */
+    pair<TreeNode *, int> dfs(TreeNode *node) {
+        if (!node) return {nullptr, 0};
+        auto l = dfs(node->left), r = dfs(node->right);
+        if (l.second == r.second) return {node, l.second + 1};
         else if (l.second > r.second) return {l.first, l.second + 1};
         else return {r.first, r.second + 1};
     }
