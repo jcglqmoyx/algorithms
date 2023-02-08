7a9aa3a101eb90927cc7bb1ae591afa2d8f003cb
Chintsai Hwo <jcglqmoyx@gmail.com>
2023-02-08 18:43:05 +0800

    update

--- leetcode/lc-us/cpp/1353.cpp
+++ leetcode/lc-us/cpp/1353.cpp
@@ -7,8 +7,8 @@ public:
     int maxEvents(vector<vector<int>> &events) {
         sort(events.begin(), events.end());
         priority_queue<int, vector<int>, greater<>> heap;
-        int n = (int) events.size(), i = 0, res = 0, day = 1;
-        while (i < n || !heap.empty()) {
+        int n = (int) events.size(), res = 0;
+        for (int i = 0, day = 1; i < n || !heap.empty(); day++) {
             while (i < n && events[i][0] == day) {
                 heap.push(events[i][1]);
                 i++;
@@ -20,7 +20,6 @@ public:
                 heap.pop();
                 res++;
             }
-            day++;
         }
         return res;
     }
