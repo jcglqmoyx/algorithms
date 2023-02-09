b20859e2b520b1144f899bbaf835a60005858456
Chintsai Hwo <jcglqmoyx@gmail.com>
2023-02-09 19:47:33 +0800

    update

--- leetcode/lc-us/cpp/1383.cpp
+++ leetcode/lc-us/cpp/1383.cpp
@@ -5,21 +5,21 @@ using namespace std;
 class Solution {
 public:
     int maxPerformance(int n, vector<int> &speed, vector<int> &efficiency, int k) {
-        using LL = long long;
+        using PII = pair<int, int>;
         const int MOD = 1e9 + 7;
 
-        vector<pair<int, int>> ve(n);
-        for (int i = 0; i < n; i++) {
-            ve[i] = {efficiency[i], speed[i]};
-        }
-        sort(ve.begin(), ve.end(), greater<>());
-        LL sum = 0, res = 0;
+        vector<PII> v(n);
+        for (int i = 0; i < n; i++) v[i] = {efficiency[i], speed[i]};
+        sort(v.begin(), v.end(), [](pair<int, int> &a, pair<int, int> &b) {
+            return a.first > b.first;
+        });
         priority_queue<int, vector<int>, greater<>> heap;
+        long long res = 0, sum = 0;
         for (int i = 0; i < n; i++) {
-            res = max(res, (sum + ve[i].second) * ve[i].first);
-            heap.push(ve[i].second);
-            sum += ve[i].second;
-            if (heap.size() == k) {
+            sum += v[i].second;
+            res = max(res, sum * v[i].first);
+            heap.push(v[i].second);
+            if (i >= k - 1) {
                 sum -= heap.top();
                 heap.pop();
             }
