6459ec934cd8b5003b8e8053a5ca7a38d009e7a3
Chintsai Hwo <jcglqmoyx@gmail.com>
2023-02-16 09:18:31 +0800

    update

--- leetcode/lc-us/cpp/1525.cpp
+++ leetcode/lc-us/cpp/1525.cpp
@@ -5,30 +5,29 @@ using namespace std;
 class Solution {
 public:
     int numSplits(string s) {
-        int n = s.length();
-        if (n == 0) return 0;
-        int count = 0;
-        int l[26] = {0}, r[26] = {0};
+        int n = (int) s.size();
+        int res = 0;
+        int l[26] = {}, r[26] = {};
         l[s[0] - 'a']++;
         for (int i = 1; i < n; i++) {
             r[s[i] - 'a']++;
         }
-        if (valid(l, r)) count++;
+        if (valid(l, r)) res++;
         for (int i = 1; i < n - 1; i++) {
             l[s[i] - 'a']++;
             r[s[i] - 'a']--;
-            if (valid(l, r)) count++;
+            if (valid(l, r)) res++;
         }
-        return count;
+        return res;
     }
 
 private:
     static bool valid(const int *l, const int *r) {
-        int count1 = 0, count2 = 0;
+        int cnt1 = 0, cnt2 = 0;
         for (int i = 0; i < 26; i++) {
-            if (l[i]) count1++;
-            if (r[i]) count2++;
+            if (l[i]) cnt1++;
+            if (r[i]) cnt2++;
         }
-        return count1 == count2;
+        return cnt1 == cnt2;
     }
 };
