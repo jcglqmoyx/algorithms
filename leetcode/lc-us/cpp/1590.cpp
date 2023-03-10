5388d1f90f4857154440607fa8b0be4830f2030d
Chintsai Hwo <jcglqmoyx@gmail.com>
2023-03-10 21:45:13 +0800

    update

--- leetcode/lc-us/cpp/1590.cpp
+++ leetcode/lc-us/cpp/1590.cpp
@@ -5,24 +5,17 @@ using namespace std;
 class Solution {
 public:
     int minSubarray(vector<int> &nums, int p) {
-        using LL = long long;
-        LL tot = 0;
-        for (int num: nums) tot += num;
-        LL remainder = tot % p;
-        if (!remainder) return 0;
-        int n = (int) nums.size();
-        int res = n;
-        unordered_map<LL, int> hash;
+        int sum = 0;
+        for (int x: nums) sum = (sum + x) % p;
+        if (!sum) return 0;
+        int n = (int) nums.size(), res = n, cur = 0;
+        unordered_map<int, int> hash;
         hash[0] = -1;
-        LL sum = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            sum += nums[i];
-            LL r = sum % p;
-            hash[r] = i;
-            LL last = (r + p - remainder) % p;
-            if (hash.count(last)) {
-                res = min(res, i - hash[last]);
-            }
+        for (int i = 0; i < n; i++) {
+            cur = (cur + nums[i]) % p;
+            int t = (cur - sum + p) % p;
+            if (hash.count(t)) res = min(res, i - hash[t]);
+            hash[cur] = i;
         }
         return res == n ? -1 : res;
     }
