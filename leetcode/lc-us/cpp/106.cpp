e5a3fdc532aed9c747ee602fb493420bf07b3cf6
Chintsai Hwo <jcglqmoyx@gmail.com>
2023-03-16 13:19:13 +0800

    update`

--- leetcode/lc-us/cpp/106.cpp
+++ leetcode/lc-us/cpp/106.cpp
@@ -15,25 +15,22 @@ struct TreeNode {
 };
 
 class Solution {
+    unordered_map<int, int> pos;
+    vector<int> post;
 public:
-    unordered_map<int, int> map;
-
     TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder) {
         int n = (int) inorder.size();
-        for (int i = 0; i < n; i++) map[inorder[i]] = i;
-        return build(inorder, postorder, 0, n - 1, 0, n - 1);
+        for (int i = 0; i < n; i++) pos[inorder[i]] = i;
+        post = std::move(postorder);
+        return construct(0, n - 1, 0, n - 1);
     }
 
-    TreeNode *build(const vector<int> &inorder, const vector<int> &postorder, int inorder_left, int inorder_right,
-                    int postorder_left, int postorder_right) {
-        if (postorder_left > postorder_right) return nullptr;
-        int inorder_root = map[postorder[postorder_right]];
-        int size_left_subtree = inorder_root - inorder_left;
-        auto *root = new TreeNode(inorder[inorder_root]);
-        root->left = build(inorder, postorder, inorder_left, inorder_root - 1, postorder_left,
-                           postorder_left + size_left_subtree - 1);
-        root->right = build(inorder, postorder, inorder_root + 1, inorder_right, postorder_left + size_left_subtree,
-                            postorder_right - 1);
+    TreeNode *construct(int il, int ir, int pl, int pr) {
+        if (pl > pr) return nullptr;
+        auto root = new TreeNode(post[pr]);
+        int idx = pos[post[pr]];
+        root->left = construct(il, idx - 1, pl, pl + idx - il - 1);
+        root->right = construct(idx + 1, ir, pl + idx - il, pr - 1);
         return root;
     }
 };
