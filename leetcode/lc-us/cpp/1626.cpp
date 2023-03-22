0cfa9eaa50bd82fe518e15dd75f496078f5e1969
Chintsai Hwo <jcglqmoyx@gmail.com>
2023-03-22 14:37:56 +0800

    update

--- leetcode/lc-us/cpp/1626.cpp
+++ leetcode/lc-us/cpp/1626.cpp
@@ -3,26 +3,34 @@
 using namespace std;
 
 class Solution {
+    int tr[1000001]{}, max_age;
+
+    int low_bit(int x) { return x & -x; }
+
+    int query(int x) {
+        int res = 0;
+        while (x) res = max(res, tr[x]), x -= low_bit(x);
+        return res;
+    }
+
+    void update(int x, int val) {
+        while (x <= max_age) tr[x] = max(tr[x], val), x += low_bit(x);
+    }
+
 public:
     int bestTeamScore(vector<int> &scores, vector<int> &ages) {
         int n = (int) scores.size();
-        vector<int> t(n);
-        for (int i = 0; i < n; i++) t[i] = i;
-        sort(t.begin(), t.end(), [&](int a, int b) {
-            if (ages[a] != ages[b]) return ages[a] < ages[b];
-            return scores[a] < scores[b];
-        });
-        for (int i = 0; i < n; i++) t[i] = scores[t[i]];
-        vector<int> f(n);
+        max_age = *max_element(ages.begin(), ages.end());
+        vector<pair<int, int>> v(n);
+        for (int i = 0; i < n; i++) {
+            v[i] = {scores[i], ages[i]};
+        }
+        sort(v.begin(), v.end());
         int res = 0;
         for (int i = 0; i < n; i++) {
-            f[i] = t[i];
-            for (int j = 0; j < i; j++) {
-                if (t[j] <= t[i]) {
-                    f[i] = max(f[i], f[j] + t[i]);
-                }
-            }
-            res = max(res, f[i]);
+            int score = v[i].first, age = v[i].second, cur = score + query(age);
+            update(age, cur);
+            res = max(res, cur);
         }
         return res;
     }
