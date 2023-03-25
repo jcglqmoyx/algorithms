c88fc82b0b752c5db98400633afb328649c09d5b
Chintsai Hwo <jcglqmoyx@gmail.com>
2023-03-25 18:25:08 +0800

    update

--- leetcode/lc-us/cpp/1574.cpp
+++ leetcode/lc-us/cpp/1574.cpp
@@ -8,25 +8,18 @@ public:
         int n = (int) arr.size();
         int l = 1, r = 1;
         for (int i = 1; i < n; i++) {
-            if (arr[i] >= arr[i - 1]) {
-                l++;
-            } else {
-                break;
-            }
+            if (arr[i] >= arr[i - 1]) l++;
+            else break;
         }
-        if (l == n) return 0;
-        for (int i = n - 2; ~i; i--) {
-            if (arr[i] <= arr[i + 1]) {
-                r++;
-            } else {
-                break;
-            }
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] <= arr[i + 1]) r++;
+            else break;
         }
-        int res = n - r;
-        for (int i = 0; i < l; i++) {
-            int p = (int) (lower_bound(arr.begin() + n - r, arr.end(), arr[i]) - arr.begin());
-            res = min(res, p + -i - 1);
+        int len = max(l, r);
+        for (int i = 0, j = n - r; i < l; i++) {
+            while (j < n && arr[j] < arr[i]) j++;
+            len = max(len, i + 1 + n - j);
         }
-        return res;
+        return max(0, n - len);
     }
 };
