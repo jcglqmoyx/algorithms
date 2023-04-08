511f4bf66397c57f4a4d7ca9c84b106c83b12410
Chintsai Hwo <jcglqmoyx@gmail.com>
2023-04-08 13:12:26 +0800

    update

--- leetcode/lc-us/cpp/1799.cpp
+++ leetcode/lc-us/cpp/1799.cpp
@@ -6,24 +6,21 @@ class Solution {
 public:
     int maxScore(vector<int> &nums) {
         int n = (int) nums.size();
-        vector<int> f(1 << n);
-        vector<vector<int>> gcd(n, vector<int>(n));
+        int f[1 << n], gcd[n][n];
+        memset(f, 0, sizeof f);
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 gcd[i][j] = __gcd(nums[i], nums[j]);
             }
         }
         for (int i = 0; i < 1 << n; i++) {
-            int cnt = 0;
-            for (int j = 0; j < n; j++) {
-                if (i >> j & 1) cnt++;
-            }
+            int cnt = __builtin_popcount(i);
             if (!(cnt & 1)) {
                 for (int j = 0; j < n; j++) {
                     if (i >> j & 1) {
                         for (int k = j + 1; k < n; k++) {
                             if (i >> k & 1) {
-                                f[i] = max(f[i], f[i - (1 << j) - (1 << k)] + ((n - cnt) / 2 + 1) * gcd[j][k]);
+                                f[i] = max(f[i], f[i ^ (1 << j) ^ (1 << k)] + ((n - cnt) / 2 + 1) * gcd[j][k]);
                             }
                         }
                     }
