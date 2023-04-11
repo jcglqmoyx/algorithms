c132c020119d5acb6111dd7184fd26f6ceadbaa9
Chintsai Hwo <jcglqmoyx@gmail.com>
2023-04-11 11:24:05 +0800

    update

--- leetcode/lc-us/cpp/1041.cpp
+++ leetcode/lc-us/cpp/1041.cpp
@@ -5,19 +5,12 @@ using namespace std;
 class Solution {
 public:
     bool isRobotBounded(string instructions) {
-        int dx[4] = {0, 1, 0, -1}, dy[4] = {1, 0, -1, 0};
-        int d = 0;
-        int x = 0, y = 0;
-        for (int i = 0; i < 4; i++) {
-            for (char c: instructions) {
-                if (c == 'G') {
-                    x += dx[d], y += dy[d];
-                } else if (c == 'L') {
-                    d = (d + 3) % 4;
-                } else if (c == 'R') {
-                    d = (d + 1) % 4;
-                }
-            }
+        int dx[] = {-1, 0, 1, 0}, dy[] = {0, -1, 0, 1};
+        int x = 0, y = 0, d = 0;
+        for (char c: instructions) {
+            if (c == 'G') x += dx[d], y += dy[d];
+            else if (c == 'L') d = (d + 1) % 4;
+            else d = (d + 3) % 4;
         }
         return x == 0 && y == 0 || d;
     }
