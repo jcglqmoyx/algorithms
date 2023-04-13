1f4c74300b5182f157c94cddee9d77a38ccf4cce
Chintsai Hwo <hqc@MacBook.local>
2023-04-13 17:10:54 +0800

    update

--- leetcode/lc-us/cpp/196.sql
+++ leetcode/lc-us/cpp/196.sql
@@ -1,5 +1,3 @@
-DELETE p2
-FROM Person p1,
-     Person p2
-WHERE p1.Email = p2.Email
-  AND P1.id < p2.id;
+DELETE
+FROM Person
+WHERE id NOT IN (SELECT * FROM (SELECT MIN(id) FROM Person GROUP BY email) t);
