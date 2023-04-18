30191b65f3c7af4b313d8c6668396662a5ba6630
Chintsai Hwo <jcglqmoyx>
2023-04-18 11:23:59 +0800

    update

--- leetcode/lc-us/cpp/1986.cpp
+++ leetcode/lc-us/cpp/1986.cpp
@@ -1,3 +1,4 @@
+/*
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -34,5 +35,35 @@ private:
         idx++;
         dfs(u + 1);
         session[--idx] -= t[u];
+    }
+};
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int minSessions(vector<int> &tasks, int sessionTime) {
+        int n = (int) tasks.size();
+        vector<pair<int, int>> f(1 << n, {INT_MAX, INT_MAX});
+        f[0] = {1, 0};
+
+        auto add = [&](const pair<int, int> &o, int x) -> pair<int, int> {
+            if (o.second + x <= sessionTime) {
+                return {o.first, o.second + x};
+            }
+            return {o.first + 1, x};
+        };
+
+        for (int mask = 1; mask < (1 << n); ++mask) {
+            for (int i = 0; i < n; ++i) {
+                if (mask & (1 << i)) {
+                    f[mask] = min(f[mask], add(f[mask ^ (1 << i)], tasks[i]));
+                }
+            }
+        }
+        return f[(1 << n) - 1].first;
     }
 };
