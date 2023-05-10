1beee359ff75d81bb37cc5459179c598ec161bea
Chintsai Hwo <jcglqmoyx>
2023-05-10 11:44:55 +0800

    update

--- leetcode/lc-us/cpp/1015.cpp
+++ leetcode/lc-us/cpp/1015.cpp
@@ -1,33 +1,9 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
 class Solution {
 public:
     int smallestRepunitDivByK(int k) {
-        int r = k % 10;
-        if (r % 2 == 0 || r % 5 == 0) return -1;
-        int len = 1;
-        int num = 1;
-        while (num < k) {
-            num = num * 10 + 1;
-            len++;
-        }
-        unordered_set<int> remainders;
-        while (true) {
-            while (num >= k) {
-                num %= k;
-                if (num == 0) {
-                    return len;
-                }
-                if (remainders.count(num)) return -1;
-                remainders.insert(num);
-                while (num < k) {
-                    num = num * 10 + 1;
-                    len++;
-                }
-            }
-        }
-        return -1;
+        if (k % 2 == 0 || k % 5 == 0) return -1;
+        int x = 1 % k, len = 1;
+        while (x) x = (x * 10 + 1) % k, len++;
+        return len;
     }
 };
