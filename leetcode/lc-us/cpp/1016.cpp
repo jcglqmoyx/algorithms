472fe4db47ade3328909f953a3c17bf82af41e61
Chintsai Hwo <jcglqmoyx>
2023-05-11 09:53:02 +0800

    update

--- leetcode/lc-us/cpp/1016.cpp
+++ leetcode/lc-us/cpp/1016.cpp
@@ -3,21 +3,23 @@
 using namespace std;
 
 class Solution {
-    string int2binary(int num) {
-        string bin;
-        while (num) {
-            bin.push_back('0' + num % 2);
-            num >>= 1;
+public:
+    bool help(const string &s, int k, int mi, int ma) {
+        unordered_set<int> st;
+        int t = 0;
+        for (int r = 0; r < s.size(); ++r) {
+            t = t * 2 + (s[r] - '0');
+            if (r >= k) t -= (s[r - k] - '0') << k;
+            if (r >= k - 1 && t >= mi && t <= ma) st.insert(t);
         }
-        reverse(bin.begin(), bin.end());
-        return bin;
+        return st.size() == ma - mi + 1;
     }
 
-public:
-    bool queryString(string S, int N) {
-        for (int i = (N >> 1) + 1; i <= N; i++) {
-            if (S.find(int2binary(i)) == string::npos) return false;
-        }
-        return true;
+    bool queryString(string s, int n) {
+        if (n == 1) return s.find('1') != -1;
+        int k = 30;
+        while ((1 << k) >= n) k--;
+        if (s.size() < (1 << (k - 1)) + k - 1 || s.size() < n - (1 << k) + k + 1) return false;
+        return help(s, k, 1 << (k - 1), (1 << k) - 1) && help(s, k + 1, 1 << k, n);
     }
 };
