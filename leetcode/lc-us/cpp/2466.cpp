6490566bc1f6683eaf8b9b6a078c34586da8fa5b
Chintsai Hwo <jcglqmoyx>
2023-05-14 09:32:34 +0800

    update

--- leetcode/lc-us/cpp/2466.cpp
+++ leetcode/lc-us/cpp/2466.cpp
@@ -6,15 +6,16 @@ class Solution {
 public:
     int countGoodStrings(int low, int high, int zero, int one) {
         const int MOD = 1e9 + 7;
+        if (zero > one) swap(zero, one);
         int f[high + 1];
         memset(f, 0, sizeof f);
         f[0] = 1;
-        int res = 0;
-        for (int i = 1; i <= high; i++) {
-            if (i >= zero) f[i] = (f[i] + f[i - zero]) % MOD;
+        for (int i = zero; i <= high; i++) {
+            f[i] = f[i - zero];
             if (i >= one) f[i] = (f[i] + f[i - one]) % MOD;
-            if (i >= low) res = (res + f[i]) % MOD;
         }
+        int res = 0;
+        for (int i = low; i <= high; i++) res = (res + f[i]) % MOD;
         return res;
     }
 };
