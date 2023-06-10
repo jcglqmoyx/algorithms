fb306757c3249a442f1bc069fa0e666167f3ad93
Chintsai Hwo <jcglqmoyx>
2023-06-10 22:28:32 +0800

    update

--- test.cpp
+++ test.cpp
@@ -2,3 +2,18 @@
 
 using namespace std;
 
+using LL = long long;
+using VI = vector<int>;
+using PII = pair<int, int>;
+using VPII = vector<pair<int, int>>;
+using VVI = vector<vector<int>>;
+using USI = unordered_set<int>;
+using UMII = unordered_map<int, int>;
+
+
+#define rep(i, a, b) for (int (i) = (a); (i) < (b); (i)++)
+#define per(i, a, b) for (int (i) = (a); (i) > (b); (i)--)
+
+const int MOD = 1e9 + 7;
+
+
