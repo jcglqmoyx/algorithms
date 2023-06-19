12724ec00454602de2d5ec7d011cb46635d50cfd
Chintsai Hwo <jcglqmoyx@gmail.com>
2023-06-19 10:21:39 +0800

    update

--- leetcode/lc-us/cpp/1262.cpp
+++ leetcode/lc-us/cpp/1262.cpp
@@ -5,34 +5,16 @@ using namespace std;
 class Solution {
 public:
     int maxSumDivThree(vector<int> &nums) {
-        int sum = 0;
-        vector<int> one, two;
+        int INF = 1e9;
+        int f[3] = {0, -INF, -INF};
         for (int x: nums) {
-            int r = x % 3;
-            sum += x;
-            if (r == 1) one.push_back(x);
-            else if (r == 2) two.push_back(x);
+            int g[3];
+            memcpy(g, f, sizeof f);
+            for (int i = 0; i < 3; i++) {
+                g[i] = max(f[i], f[((i - x) % 3 + 3) % 3] + x);
+            }
+            memcpy(f, g, sizeof g);
         }
-        sort(one.begin(), one.end());
-        sort(two.begin(), two.end());
-        int res = 0;
-        int r = sum % 3;
-        if (r == 0) res = sum;
-        else if (r == 1) {
-            if (!one.empty()) {
-                res = max(res, sum - one[0]);
-            }
-            if (two.size() >= 2) {
-                res = max(res, sum - two[0] - two[1]);
-            }
-        } else {
-            if (one.size() >= 2) {
-                res = max(res, sum - one[0] - one[1]);
-            }
-            if (!two.empty()) {
-                res = max(res, sum - two[0]);
-            }
-        }
-        return res;
+        return f[0];
     }
 };
